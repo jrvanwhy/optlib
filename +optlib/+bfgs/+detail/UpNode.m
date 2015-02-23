340ff707f63f0f94bfaaf144454aa4b44e5f7be0
Johnathan Van Why <jrvanwhy@gmail.com>
2015-02-23 00:03:33 -0800

    Performance improvement for the L-BFGS implementation.

--- +optlib/+bfgs/+detail/UpNode.m
+++ +optlib/+bfgs/+detail/UpNode.m
@@ -4,8 +4,6 @@ classdef UpNode < handle
 			this.oldTree = oldTree;
 			this.x       = x;
 			this.Mx      = Mx;
-			this.Bx      = oldTree * x;
-			this.xT_Bx   = x.' * this.Bx;
 
 			if nargin < 4 || isempty(dotP)
 				this.MxT_x = Mx.' * x;
@@ -20,6 +18,13 @@ classdef UpNode < handle
 		end
 
 		function out = mtimes(this, vec)
+			% These aren't always calculated, so initialize them if necessary
+			if isempty(this.Bx)
+				this.Bx    = this.oldTree * this.x;
+				this.xT_Bx = this.x.' * this.Bx;
+			end
+
+			% Here's the actual calculation
 			out = this.oldTree * vec + this.Mx * ((this.Mx.' * vec) / this.MxT_x) - this.Bx * ((this.Bx.' * vec) / this.xT_Bx);
 		end
 	end
@@ -27,10 +32,10 @@ classdef UpNode < handle
 	properties
 		oldTree % The old LBFGS data tree
 
-		x     % The x vector from the M*x update
-		Mx    % The product between the actual matrix and the step (x).
-		MxT_x % Dot product of Mx and x
-		Bx    % Product of the old B matrix and x
-		xT_Bx % Dot product between x and Bx (curvature information)
+		x          % The x vector from the M*x update
+		Mx         % The product between the actual matrix and the step (x).
+		MxT_x      % Dot product of Mx and x
+		Bx    = [] % Product of the old B matrix and x
+		xT_Bx = [] % Dot product between x and Bx (curvature information)
 	end
 end
