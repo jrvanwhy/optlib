eff254c16a22021735e1b47c1820fc2f068357a5
Johnathan Van Why <jrvanwhy@gmail.com>
2015-02-23 14:18:59 -0800

    Bump this up to compare better with IPOPT.

--- tests/testBigRosen.m
+++ tests/testBigRosen.m
@@ -1,5 +1,5 @@
 %% Test 1: Large multi-variable Rosenbrock function minimization with a poor initial guess.
-x = sym('x', [100 1]);
+x = sym('x', [400 1]);
 obj   = sum(100*(x(2:end) - x(1:end-1).^2).^2 + (1 - x(1:end-1)).^2);
 soln  = sym_minunc(obj, x, 10 * ones(numel(x), 1));
 assert(all(abs(soln - ones(numel(soln), 1)) < 1e-6))
