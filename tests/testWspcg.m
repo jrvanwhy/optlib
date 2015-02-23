95cf59bc42629b62966df9367aac8d3f8e8bfb0a
Johnathan Van Why <jrvanwhy@gmail.com>
2015-02-22 17:58:24 -0800

    Got it figured out... the residual that I couldn't get rid of with iterative refinement corresponded to crummy eigenvalues of the system.
    Will this be a problem for optimization?

--- tests/testWspcg.m
+++ tests/testWspcg.m
@@ -1,9 +1,11 @@
 clear all
 
 M = hilb(1000);
-b = M * ones(size(M, 2), 1);
+b1 = M * ones(size(M, 2), 1);
+b2 = M * (1:size(M, 2)).';
 
 wspcg = optlib.ws_pcg.WSPCG;
 
-[x1, mindotP1] = wspcg.solve(@(x) M * x, b, diag(M), sqrt(eps));
-[dx1,mindotP2] = wspcg.solve(@(x) M * x, b - M * x1, diag(M), .01);
+[x1, mindotP1] = wspcg.solve(@(x) M * x, b1, diag(M), sqrt(eps));
+[x2, mindotP2] = wspcg.solve(@(x) M * x, b2, diag(M), sqrt(eps));
+[x3, mindotP3] = wspcg.solve(@(x) M * x, b1, diag(M), sqrt(eps));
