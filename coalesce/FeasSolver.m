e60abe9d9305941d9e19f5c4cb2ae4e163561422
Johnathan Van Why <jrvanwhy@gmail.com>
2015-03-12 21:05:10 -0700

    FeasSolver now handles arbitrary equality constraints.

--- coalesce/FeasSolver.m
+++ coalesce/FeasSolver.m
@@ -1,9 +1,8 @@
 % COALESCE interface for solving feasibility problems.
 %
 % TODO:
-%     * Equality constraints
 %     * Inequality constraints (other than bounds)
-%     * Robustness (i.e. nonfinite constraint values
+%     * Robustness (i.e. nonfinite constraint values)
 
 classdef FeasSolver < Solver
 	methods
@@ -16,6 +15,12 @@ classdef FeasSolver < Solver
 			% User feedback
 			disp('Exporting FeasSolver functions')
 
+			% Clear out old functions so that MATLAB will recognize our new ones
+			clear feasCeq feasJCeq
+
+			% These are used repeatedly later
+			nVars = this.nlp.numberOfVariables;
+
 			% Identify equality constraints
 			conIsEq = [];
 			for iter = 1:numel(this.nlp.constraint)
@@ -23,16 +28,29 @@ classdef FeasSolver < Solver
 			end
 			conIsEq = logical(conIsEq);
 
-			% Generate the equality constraints function
-			gen = MatlabFunctionGenerator({'var'}, {'ceq'}, 'feasCeq');
-			gen.writeHeader;
-			% Check for a lack of equality constraints -- we need to special case or the referencing will fail
+			% Generate the equality constraints functions
+			% First, check for a lack of equality constraints -- we need to special case or the referencing will fail
 			if any(conIsEq)
-				ceq = vertcat(this.nlp.constraint(conIsEq).expression - this.nlp.constraint(conIsEq).lowerBound);
+				eqCons = this.nlp.constraint(conIsEq);
+				for iter = 1:nnz(conIsEq)
+					ceq(iter) = eqCons(iter).expression - eqCons(iter).lowerBound;
+				end
 			else
 				ceq = ConstantNode.empty;
 			end
+			% Generate the constraint function itself
+			gen = MatlabFunctionGenerator({'var'}, {'ceq'}, 'feasCeq');
+			gen.writeHeader
 			gen.writeExpression(ceq, 'ceq')
+			gen.writeFooter
+			% Generate the ceq Jacobian function
+			gen = MatlabFunctionGenerator({'var'}, {'jceq'}, 'feasJCeq');
+			gen.writeHeader
+			[iJeq, jJeq, sJeq] = ceq.jacobian;
+			gen.writeIndex(iJeq, 'iJeq')
+			gen.writeIndex(jJeq, 'jJeq')
+			gen.writeExpression(sJeq, 'sJeq')
+			fprintf(gen.fid, '\tjceq = sparse(iJeq, jJeq, sJeq, %d, %d);\n', nVars, sum([ceq.length]));
 			gen.writeFooter
 		end
 
@@ -45,36 +63,58 @@ classdef FeasSolver < Solver
 
 			% Formulate the initial guess. Clamp as necessary between the lower and upper bounds
 			% (so that we always satisfy the bounds throughout the solution process).
-			cur_x = min(max(this.nlp.initialGuess, this.nlp.variableLowerBound), this.nlp.variableUpperBound);
+			cur_x = min(max(this.nlp.initialGuess(:), this.nlp.variableLowerBound(:)), this.nlp.variableUpperBound(:));
 
 			% Was the solve successful? This gets set to true if the termination criteria are met.
 			success = false;
 
 			% Initialize saved values
-			meritVal = this.meritFcn(cur_x);
+			ceqVal   = feasCeq(cur_x);
+			meritVal = this.meritFcn(ceqVal);
 
 			% Optimization main loop
 			for iter = 1:this.maxIter
-				% TODO: Setup!
-				objjac = zeros(numel(cur_x), 1);
-				A      = [];
-				b      = [];
-				Aeq    = [];
-				beq    = [];
+				% Evaluate the derivative for use in assembling the LP subproblem
+				jCeq = feasJCeq(cur_x);
+
+				% Put together the LP subproblem
+
+				% Objective first. This is just a sum of slack variables
+				objjac = [ zeros(numel(cur_x), 1)
+				           ones(numel(ceqVal), 1) ];
+
+				% Put together the slack variable definition constraints
+				A = [ jCeq, -eye(numel(ceqVal))
+				     -jCeq, -eye(numel(ceqVal)) ];
+
+				b = [ -ceqVal
+				       ceqVal ];
+
+				% Check for corner cases
+				if isempty(b)
+					A = [];
+				end
 
 				% Compute the lower bounds. Since the LP is relative, these are shifted with respect
 				% to the real upper and lower bounds. TODO: Add in the bounds on the slack variables...
-				lp_lb = this.nlp.variableLowerBound - cur_x;
-				lp_ub = this.nlp.variableUpperBound - cur_x;
+				lp_lb = [ this.nlp.variableLowerBound(:) - cur_x
+				          -inf(numel(ceqVal), 1) ];
+
+				lp_ub = [ this.nlp.variableUpperBound(:) - cur_x
+				          abs(ceqVal) ];
 
 				% Compute the step and desired objective function decrease
-				[step, desobjchg] = this.callLPSolver(objjac, A, b, Aeq, beq, lp_lb, lp_ub);
+				[lp_step, desobjchg] = this.callLPSolver(objjac, A, b, lp_lb, lp_ub);
+
+				% Pull out just the x step
+				step = lp_step(1:numel(cur_x));
 
 				% Line search!
 				slen = 1;
 				while true
 					new_x       = cur_x + slen * step;
-					newMeritVal = this.meritFcn(new_x);
+					newCeqVal   = feasCeq(new_x);
+					newMeritVal = this.meritFcn(newCeqVal);
 
 					if newMeritVal <= meritVal + slen * desobjchg / 2
 						break
@@ -85,6 +125,7 @@ classdef FeasSolver < Solver
 
 				% Update the current point and other associated values
 				cur_x    = new_x;
+				ceqVal   = newCeqVal;
 				meritVal = newMeritVal;
 
 				% Termination check!
@@ -106,19 +147,19 @@ classdef FeasSolver < Solver
 		% This calls the linear programming solver behind the scenes.
 		% Assumes the initial guess is the zero vector (useful for relative step computation)
 		% This is intended to interface to different LP solvers
-		function [soln,objval] = callLPSolver(this, objjac, A, b, Aeq, beq, lb, ub)
-			[soln,objval] = linprog(objjac, A, b, Aeq, beq, lb, ub, [], this.lpoptions);
+		function [soln,objval] = callLPSolver(this, objjac, A, b, lb, ub)
+			[soln,objval] = linprog(objjac, A, b, [], [], lb, ub, [], this.lpoptions);
 		end
 
 		% Merit function for the optimization.
-		function val = meritFcn(this, x)
-			val = sum(abs(feasCeq(x)));
+		function val = meritFcn(this, ceqVal)
+			val = sum(abs(ceqVal));
 		end
 	end
 
 	properties
 		% Solver parameters
-		maxIter   = 3         % Maximum number of iterations during the solution
+		maxIter   = 100       % Maximum number of iterations during the solution
 		tolerance = sqrt(eps) % Termination tolerance for the solver
 
 		% Options for the LP solver
--- tests/testFeasSolver.m
+++ tests/testFeasSolver.m
@@ -1,4 +1,5 @@
 %% Test 1: Basic bound-constrained feasibility problem.
+clear all
 prob  = Nlp;
 x     = prob.addVariable(0, 1, 2);
 optim = FeasSolver(prob);
@@ -8,19 +9,48 @@ assert(x.solution >= 1)
 assert(x.solution <= 2)
 
 %% Test 2: Add in an equality constraint -- which is satisfied anyway by interior-point methods due to bounds choice.
+clear all
 prob  = Nlp;
 x     = prob.addVariable(0, 1, 2);
 prob.addConstraint(1.5, x, 1.5)
 optim = FeasSolver(prob);
 optim.export
 optim.solve
-assert(x.solution == 1.5)
+assert(abs(x.solution - 1.5) < sqrt(eps))
 
 %% Test 3: Problem with an equality constraint not midway between the upper and lower bounds
+clear all
 prob = Nlp;
 x    = prob.addVariable(0, -inf, inf);
-prob.addConstraint(1, x, 1);
+prob.addConstraint(1, x, 1)
 optim = FeasSolver(prob);
 optim.export
 optim.solve
-assert(x.solution == 1)
+assert(abs(x.solution - 1) < sqrt(eps))
+
+%% Test 4: First nonlinear problem. Actually a bit difficult -- Newton's method fails with this initial guess, even damped
+clear all
+prob = Nlp;
+x    = prob.addVariable(0, -inf, inf);
+y    = prob.addVariable(0, -inf, inf);
+prob.addConstraint(2, x + y, 2)
+prob.addConstraint(1, x * y, 1)
+optim = FeasSolver(prob);
+optim.export
+optim.solve
+assert(abs(x.solution - 1) < sqrt(eps))
+assert(abs(y.solution - 1) < sqrt(eps))
+
+%% Test 5: Sequentially solve a series of equations. Similar to the last, but it shouldn't accidentally stumble across the solution in 1 iteration
+clear all
+prob = Nlp;
+x    = prob.addVariable(0, -inf, inf, 'Length', 10);
+expr = 1;
+for iter = 1:x.length
+	expr = expr * ind(x, iter);
+	prob.addConstraint(1, expr, 1)
+end
+optim = FeasSolver(prob);
+optim.export
+optim.solve
+assert(all(abs(x.solution - 1) < sqrt(eps)))
