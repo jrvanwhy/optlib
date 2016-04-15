46967d0b91b9ce845e6c26d475d4a72360a895c3
Johnathan Van Why <jrvanwhy@gmail.com>
2016-04-14 23:04:03 -0700

    Fix the PCG text script

--- tests/testWspcg.m
+++ tests/testWspcg.m
@@ -6,6 +6,7 @@ b2 = M * (1:size(M, 2)).';
 
 wspcg = optlib.ws_pcg.WSPCG;
 
-[x1, mindotP1] = wspcg.solve(@(x) M * x, b1, diag(M), sqrt(eps));
-[x2, mindotP2] = wspcg.solve(@(x) M * x, b2, diag(M), sqrt(eps));
-[x3, mindotP3] = wspcg.solve(@(x) M * x, b1, diag(M), sqrt(eps));
+tol = 10^-12;
+[x1, mindotP1] = wspcg.solve(@(x) M * x, b1, diag(M), tol, 1000);
+[x2, mindotP2] = wspcg.solve(@(x) M * x, b2, diag(M), tol, 1000);
+[x3, mindotP3] = wspcg.solve(@(x) M * x, b1, diag(M), tol, 1000);
